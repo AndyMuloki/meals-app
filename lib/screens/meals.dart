99d4db90365d839f8ec7abb1aa9aeec632f0f7c6
Andrew <andrew.muloki@strathmore.edu>
2023-05-05 19:24:28 +0300

    add tab-based navigation

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -7,11 +7,11 @@ import 'package:meals/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -47,19 +47,6 @@ class MealsScreen extends StatelessWidget {
       ),
     );
 
-// finished product
-    // if (meals.isNotEmpty) {
-    //   content = ListView.builder(
-    //     itemCount: meals.length,
-    //     itemBuilder: (ctx, index) => MealItem(
-    //       meal: meals[index],
-    //       onSelectedMeal: (meal) {
-    //         selectMeal(context, meal);
-    //       },
-    //     ),
-    //   );
-    // }
-
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
@@ -72,9 +59,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
